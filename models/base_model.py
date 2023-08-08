5b5acc1a84114d24de4f39a89f7c2db2117fe782
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-08 22:36:09 +0200

    args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,20 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
-        """init magic method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """init magic method
+
+        Args:
+            args: is a Tuple that contains all arguments (unused)
+            kwargs:  is a dictionary that contains all arguments by key/value
+        """
+        if len(kwargs) == 0 and kwargs is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                pass
 
     def __str__(self):
         """string representation of instance
