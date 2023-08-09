020b9cf8053ab89a3df9e9672ede11349cb4878a
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-09 21:28:41 +0200

    updae file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,14 +7,23 @@ import json
 
 class FileStorage:
     """class sued for serialization and deserialization
+
+    Attrs:
+        file_path: path to the JSON file
+        objects: dictionary to store all objects
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns the dictionary objects
+        """
         return self.__objects
 
     def new(self, obj):
+        """sets in objects attribute the obj 
+        with calssName.<id> as key
+        """
         # self.__objects[key] = obj
         # key = <obj class name>.id
         # classname.id  ->     ERROR
