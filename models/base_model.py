8623bcce872f98ff3994c1265da152902f00398d
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-09 22:26:32 +0200

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,8 @@ class BaseModel:
             kwargs:  is a dictionary that contains all arguments by key/value
         """
         dt_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) == 0 and kwargs is None:
+        # if len(kwargs) == 0:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
