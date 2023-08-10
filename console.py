142625d4b06a214792f022c70df34810ca36de49
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-11 01:32:09 +0200

    update show method

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 import os
 from models.all_models import models
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -35,9 +36,6 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """creates new instace as save it to json file
         """
-        # print(line)
-        # args = line.split()
-        # print(args)
         if not line:
             print("** class name missing **")
         elif line not in models.keys():
@@ -47,17 +45,17 @@ class HBNBCommand(cmd.Cmd):
             # print(new_obj.id)
             # new_obj.save()
             my_model = models[line]()
-            my_model.save()
             print(my_model.id)
-
+            my_model.save()
 
     def do_show(self, line):
         args = line.split()
-        if not args[0]:
+        # print(args)
+        if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.our_classes:
+        elif args[0] not in models.keys():
             print("** class doesn't exist **")
-        elif not args[1]:
+        elif len(args) == 1:
             print("** instance id missing **")
         else:
             pass
