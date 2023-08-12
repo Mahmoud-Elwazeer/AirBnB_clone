70750a0f638d5b8a13fc03d0b804802f7dacce41
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-12 20:34:12 +0300

    update time

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -32,15 +32,45 @@ class TestBaseModel(unittest.TestCase):
         my_model = base_model.BaseModel()
         self.assertIsInstance(my_model.id, str)
 
+    def test_unique_id(self):
+        """test if the id is unique"""
+        b1 = base_model.BaseModel()
+        b2 = base_model.BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_kwargs_id(self):
+        """check value of id in kwargs"""
+        kwargs = {
+            'id': '123'
+        }
+        my_model = base_model.BaseModel(**kwargs)
+        self.assertEqual(my_model.id, '123')
+
     def test_datatime_created_at(self):
         """test created_at -> datetime"""
         my_model = base_model.BaseModel()
         self.assertIsInstance(my_model.created_at, datetime)
 
+    def test_kwargs_created_at(self):
+        """check created_at convert from string to datetime"""
+        kwargs = {
+            'created_at': '2023-08-11T18:28:19.430925'
+        }
+        my_model = base_model.BaseModel(**kwargs)
+        self.assertEqual(my_model.created_at.year, 2023)
+
     def test_datatime_updated_at(self):
         """test updated_at -> datetime"""
         my_model = base_model.BaseModel()
         self.assertIsInstance(my_model.updated_at, datetime)
+
+    def test_kwargs_updated_at(self):
+        """check updated_at convert from string to datetime"""
+        kwargs = {
+            'updated_at': '2023-08-11T18:28:19.430963'
+        }
+        my_model = base_model.BaseModel(**kwargs)
+        self.assertEqual(my_model.updated_at.year, 2023)
 
     def test_str_method(self):
         """testing __str__ representation"""
@@ -48,9 +78,10 @@ class TestBaseModel(unittest.TestCase):
         utput = f"[BaseModel] ({my_model.id}) {my_model.__dict__}"
         self.assertEqual(my_model.__str__(), utput)
 
-    def test_unique_id(self):
-        """test if the id is unique"""
-        b1 = base_model.BaseModel()
-        b2 = base_model.BaseModel()
+    def test_save_updatetime(self):
+        """check save update time or not"""
+        my_model = base_model.BaseModel()
+        old_date = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(old_date, my_model.updated_at)
 
-        self.assertNotEqual(b1.id, b2.id)
