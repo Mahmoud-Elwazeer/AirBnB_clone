59b401deb2bf34f84ef87bbc8a0825e4dc03bd31
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-12 19:51:55 +0300

    unittest BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,7 +3,7 @@
 """
 from models import base_model
 import unittest
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     """test cases for basemodel class
@@ -22,11 +22,31 @@ class TestBaseModel(unittest.TestCase):
         for method in dir(base_model.BaseModel):
             self.assertTrue(len(method.__doc__) > 0)
 
+    def test_init(self):
+        """test create object from BaseModel"""
+        my_model = base_model.BaseModel()
+        self.assertIsInstance(my_model , base_model.BaseModel)
+    
+    def test_str_id(self):
+        """test id -> str"""
+        my_model = base_model.BaseModel()
+        self.assertIsInstance(my_model.id, str)
+
+    def test_datatime_created_at(self):
+        """test created_at -> datetime"""
+        my_model = base_model.BaseModel()
+        self.assertIsInstance(my_model.created_at, datetime)
+
+    def test_datatime_updated_at(self):
+        """test updated_at -> datetime"""
+        my_model = base_model.BaseModel()
+        self.assertIsInstance(my_model.updated_at, datetime)
+
     def test_str_method(self):
         """testing __str__ representation"""
-        b = base_model.BaseModel()
-        expected_output = f"[BaseModel] ({b.id}) {b.__dict__}"
-        self.assertEqual(b.__str__(), expected_output)
+        my_model = base_model.BaseModel()
+        utput = f"[BaseModel] ({my_model.id}) {my_model.__dict__}"
+        self.assertEqual(my_model.__str__(), utput)
 
     def test_unique_id(self):
         """test if the id is unique"""
