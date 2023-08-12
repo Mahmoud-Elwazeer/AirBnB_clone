29758162f420291b43618ee36cb1acfeeade4ebe
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-12 23:23:24 +0200

    update filestorge test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 """Module for file_storage test Cases
 """
-from models.engine.file_storage import FileStorage
+from models.engine import file_storage
 import unittest
 
 
 class TestFileStorage(unittest.TestCase):
-    pass
+    """test cases for FileStorage class
+    """
+
+    def test_module_documetation(self):
+        """test the documentation of a module
+        """
+        self.assertTrue(len(file_storage.__doc__) > 0)
+
+    def test_class_documetation(self):
+        """test the documentation of class
+        """
+        self.assertTrue(len(file_storage.FileStorage.__doc__) > 0)
+
+    def test_method_documetation(self):
+        """test documentation of methods inside and outside class
+        """
+        for method in dir(file_storage):
+            self.assertTrue(len(method.__doc__) > 0)
